63e878b653a709215ad8166c0ae077e52c85d7db
jgciudad <59373672+jgciudad@users.noreply.github.com>
2022-04-07 22:44:55 +0200

    Add files via upload

--- my_code/data_loading_facades.py
+++ my_code/data_loading_facades.py
@@ -15,6 +15,10 @@ import numpy as np
 # from torch.utils.data import DataLoader
 # import SimpleITK as sitk
 import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import torchvision.transforms.functional as TF
+import random
+
 
 
 folder_path = r'C:\Users\javig\Documents\DTU data (not in drive)\GANs data\CMP_facade_DB_base\base/'
@@ -37,19 +41,29 @@ class FacadeDataset(data.Dataset):
         # folder_path = folder_path.replace(os.sep, '/')
         self.facade_files = sorted(glob.glob(os.path.join(folder_path,'*.jpg')))
         self.layout_files = sorted(glob.glob(os.path.join(folder_path,'*.png')))
+        # self.trans = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
+        #                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
         # self.CT_size = CT_size
       # self.MR_size = MR_size
+      
+    def transform(self, input_tensor, target_tensor):
+        # Random horizontal flipping
+        if random.random() > 0.5:
+            input_tensor = TF.hflip(input_tensor)
+            target_tensor = TF.hflip(target_tensor)
+            
+        input_tensor = TF.normalize(input_tensor,(0.5), (0.5))
+        target_tensor = TF.normalize(target_tensor,(0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        
+        return input_tensor, target_tensor
 
     def __getitem__(self, index, img_size = (256,256)):
         facade_path = self.facade_files[index]
         layout_path = self.layout_files[index]
         facade_img = Image.open(facade_path).resize(img_size)
         layout_img = Image.open(layout_path).resize(img_size)
-        # facade_img = np.array(facade_img)/255
-        # layout_img = np.array(layout_img)/255
-        facade_img = (np.array(facade_img) / 127.5) - 1
-        layout_img = (np.array(layout_img) / 127.5) - 1
-
+        facade_img = np.array(facade_img)/255
+        layout_img = np.array(layout_img)/255
         # MR_image = MR_image.reshape([MR_image.shape[2],MR_image[0],MR_image[1]])
      
         # if len(image.shape) ==2:
@@ -57,8 +71,13 @@ class FacadeDataset(data.Dataset):
         # if len(image.shape) > 2 and image.shape[2] == 4:
         #     #if the image is .png (has 4 channels) convert the image from RGBA2RGB
         #     image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+        
+        layout_tensor = torch.from_numpy(layout_img).float().unsqueeze(0)
+        facade_tensor =  torch.from_numpy(facade_img).permute(2,0,1).float()
+        
+        layout_tensor, facade_tensor = self.transform(layout_tensor, facade_tensor)
     
-        return torch.from_numpy(layout_img).float().unsqueeze(0), torch.from_numpy(facade_img).permute(2,0,1).float(), facade_path
+        return layout_tensor, facade_tensor, facade_path
     
     def __len__(self):
         return len(self.facade_files)
@@ -69,18 +88,17 @@ def segment_dataset_and_save(destination_folder, dataloader, model, device):
     for i, buildings_data in enumerate(dataloader, 0):
     
         layout_images, facade_images, facade_paths = buildings_data
-                   
+                          
         output = model(layout_images.to(device))
+        output = TF.normalize(output,(-1, -1, -1),(1/0.5, 1/0.5, 1/0.5))
         output_numpy = np.transpose(output.cpu().detach().numpy(),(0,2,3,1))
-                
+        # output_numpy = output_numpy*255
         
         facade_numpy = np.transpose(facade_images.detach().numpy(),(0,2,3,1))
-        # facade_numpy = facade_numpy * 255
-        facade_numpy = (facade_numpy + 1) * 127.5
+        facade_numpy = facade_numpy * 255
         
         for k, out_img in enumerate(output_numpy):
-           # out_img = (out_img * 255).astype(np.uint8)
-           out_img = ((out_img + 1) * 127.5).astype(np.uint8)
+           out_img = (out_img * 255).astype(np.uint8)
            im = Image.fromarray(out_img)
            
            paired_im = np.hstack((facade_numpy[k,:,:,:], 255*np.ones((output_numpy[k,:,:,:].shape[0], 50, 3)), im))
