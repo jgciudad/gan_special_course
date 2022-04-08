acae98443231c37e5fc8dd21eb4ad3448a899d53
jgciudad <jgciudad@outlook.com>
2022-04-08 13:36:03 +0200

    Update data_loading_facades.py

--- my_code/data_loading_facades.py
+++ my_code/data_loading_facades.py
@@ -38,20 +38,24 @@ class FacadeDataset(data.Dataset):
     
     def __init__(self, folder_path): #, CT_size, MR_size):
         super(FacadeDataset, self).__init__()
-        # folder_path = folder_path.replace(os.sep, '/')
+
         self.facade_files = sorted(glob.glob(os.path.join(folder_path,'*.jpg')))
         self.layout_files = sorted(glob.glob(os.path.join(folder_path,'*.png')))
+        
         # self.trans = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
         #                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        
         # self.CT_size = CT_size
-      # self.MR_size = MR_size
+        # self.MR_size = MR_size
       
     def transform(self, input_tensor, target_tensor):
+        
         # Random horizontal flipping
         if random.random() > 0.5:
             input_tensor = TF.hflip(input_tensor)
             target_tensor = TF.hflip(target_tensor)
-            
+        
+        # Normalization
         input_tensor = TF.normalize(input_tensor,(0.5), (0.5))
         target_tensor = TF.normalize(target_tensor,(0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         
@@ -82,6 +86,7 @@ class FacadeDataset(data.Dataset):
         layout_tensor = torch.from_numpy(layout_img).float().unsqueeze(0)
         facade_tensor =  torch.from_numpy(facade_img).permute(2,0,1).float()
         
+        # Random flipping and normalization
         layout_tensor, facade_tensor = self.transform(layout_tensor, facade_tensor)
     
         return layout_tensor, facade_tensor, facade_path
@@ -97,27 +102,24 @@ def segment_dataset_and_save(destination_folder, dataloader, model, device):
         layout_images, facade_images, facade_paths = buildings_data
                    
         output = model(layout_images.to(device))
-        output = TF.normalize(output,(-1, -1, -1),(1/0.5, 1/0.5, 1/0.5))
+        output = TF.normalize(output,(-1, -1, -1),(1/0.5, 1/0.5, 1/0.5)) # Undo normalization
         output_numpy = np.transpose(output.cpu().detach().numpy(),(0,2,3,1))
                 
         
         facade_numpy = np.transpose(facade_images.detach().numpy(),(0,2,3,1))
-        # facade_numpy = facade_numpy * 255
-        facade_numpy = (facade_numpy + 1) * 127.5
+        # facade_numpy = facade_numpy * 255  # undo [0,1] normalization 
+        facade_numpy = (facade_numpy + 1) * 127.5 # undo [-1,1] normalization 
         
         for k, out_img in enumerate(output_numpy):
-           # out_img = (out_img * 255).astype(np.uint8)
-           out_img = ((out_img + 1) * 127.5).astype(np.uint8)
+           # out_img = (out_img * 255).astype(np.uint8) # undo [0,1] normalization 
+           out_img = ((out_img + 1) * 127.5).astype(np.uint8) # undo [-1,1] normalization 
            im = Image.fromarray(out_img)
            
            paired_im = np.hstack((facade_numpy[k,:,:,:], 255*np.ones((output_numpy[k,:,:,:].shape[0], 50, 3)), im))
            paired_im = Image.fromarray(paired_im.astype(np.uint8))
                         
            im_path = os.path.join(destination_folder,'images',facade_paths[k].split(os.sep)[-1][:-4]+'.jpg')
-           # im_path = im_path.replace(os.sep,'/')
-           
            paired_im_path = os.path.join(destination_folder,'paired_images',facade_paths[k].split(os.sep)[-1][:-4]+'.jpg')
-           # paired_im_path = paired_im_path.replace(os.sep,'/')
                    
            if not os.path.exists(os.path.dirname(im_path)):
                os.makedirs(os.path.dirname(im_path))
